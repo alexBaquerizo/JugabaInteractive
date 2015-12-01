acaadfbe34f6c3c78752fb9992898afe3a859624
alexBaquerizo <elbatracio_21@hotmail.com>
2015-12-02 00:17:27 +0100

    servicios

--- public/js/servicios.js
+++ public/js/servicios.js
@@ -1,142 +1,15 @@
 
-
-function muestra1() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="visible";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="oculto";
-
+window.onload = function(){
+	muestra;
 }
 
+function muestra(contenido) {
+
+	var contenidos = ["contenido1", "contenido2", "contenido3", "contenido4", "contenido5", "contenido6", "contenido7"];
+
+	document.getElementById('info_servicios').style.display = "none";
  
-function muestra2() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="visible";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="oculto";
-
-}
-
-function muestra3() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="visible";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="oculto";
-
-}
-
-function muestra4() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="visible";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="oculto";
-
-}
-
-function muestra5() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="visible";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="oculto";
-
-}
-
-function muestra6() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="visible";
-	Contenido_Otros.className="oculto";
-
-}
-
-function muestra7() {
-	var Contenido_Videojuegos = document.getElementById("desarrollo_videojuegos");
-	var Contenido_EntornosInteractivos = document.getElementById("entornos_interactivos");
-	var Contenido_Publicidad = document.getElementById("publi");
-	var Contenido_Cortos = document.getElementById("cortos_animados");
-	var Contenido_Aplicaciones = document.getElementById("aplicaciones");
-	var Contenido_Visualizacion = document.getElementById("visualizacion_3d");
-	var Contenido_Otros = document.getElementById("otros");
-
-
-	Contenido_Videojuegos.className="oculto";
-	Contenido_EntornosInteractivos.className="oculto";
-	Contenido_Publicidad.className="oculto";
-	Contenido_Cortos.className="oculto";
-	Contenido_Aplicaciones.className="oculto";
-	Contenido_Visualizacion.className="oculto";
-	Contenido_Otros.className="visible";
-
+	for (i = 0, total = contenidos.length; i < total; i ++) {
+		document.getElementById(contenidos[i]).style.display = (contenidos[i] == contenido) ? "block":"none"; 
+	} 
 }
